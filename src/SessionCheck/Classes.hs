94e54cab63471e2de1be24ad8fcd1e166752c9f5
Maximilian Algehed <m.algehed@gmail.com>
2017-08-15 16:33:04 +0200

    added instances to <

--- src/SessionCheck/Classes.hs
+++ src/SessionCheck/Classes.hs
@@ -1,8 +1,17 @@
 {-# LANGUAGE TypeOperators
            , MultiParamTypeClasses
+           , FlexibleInstances
 #-}
 module SessionCheck.Classes where
 
 class (a :< b) where
   inj :: a -> b
   prj :: b -> Maybe a
+
+instance a :< a where
+  inj = id
+  prj = Just 
+
+instance a :< Maybe a where
+  inj = Just
+  prj = id
