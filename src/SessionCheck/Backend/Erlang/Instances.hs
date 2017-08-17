c74564b9ebef851491549975b822067783747cef
Maximilian Algehed <m.algehed@gmail.com>
2017-08-17 13:47:27 +0200

    Erlang backend

---
+++ src/SessionCheck/Backend/Erlang/Instances.hs
@@ -0,0 +1,99 @@
+{-# LANGUAGE TypeOperators
+           , FlexibleInstances
+           , FlexibleContexts
+           , MultiParamTypeClasses
+           , UndecidableInstances #-}
+module SessionCheck.Backend.Erlang.Instances ( ErlType ) where
+
+import Foreign.Erlang
+import Data.Char
+import Data.Maybe
+
+import SessionCheck.Classes
+
+instance Int :< ErlType where
+    inj x             
+       | abs x <= 0x7FFFFFFF = ErlInt x        
+       | otherwise           = ErlBigInt (fromIntegral x) -- Haskell Int (might) use 64 bits whether erlang's small Int use only 32 bit
+
+    prj (ErlInt x)    = Just x
+    prj (ErlBigInt x) = Just $ fromIntegral x
+    prj _ = Nothing
+
+instance Double :< ErlType where
+    inj   x            = ErlFloat x 
+    prj (ErlFloat x) = Just x
+    prj _ = Nothing
+
+instance Float :< ErlType where
+    inj x              = ErlFloat (realToFrac x)
+    prj (ErlFloat x) = Just $ realToFrac x
+    prj _ = Nothing
+
+instance Integer :< ErlType where
+    inj   x             = ErlBigInt x
+    prj (ErlInt x)    = Just $ fromIntegral x
+    prj (ErlBigInt x) = Just x
+    prj _ = Nothing
+
+instance String :< ErlType where
+    inj   x           = ErlString x
+    prj ErlNull       = Just $ ""
+    prj (ErlString x) = Just $ x
+    prj (ErlAtom x)   = Just $ x
+    prj (ErlList xs)  = Just $ map (chr . fromErlang) xs
+    prj _ = Nothing
+
+instance Bool :< ErlType where
+    inj   True              = ErlAtom "true"
+    inj   False             = ErlAtom "false"
+    prj (ErlAtom "true")  = Just True
+    prj (ErlAtom "false") = Just False
+    prj _ = Nothing
+
+instance [ErlType] :< ErlType where
+    inj   []           = ErlNull
+    inj   xs           = ErlList xs
+    prj ErlNull      = Just []
+    prj (ErlList xs) = Just xs
+    prj _ = Nothing
+
+instance {-# OVERLAPPABLE #-} (a :< ErlType) => [a] :< ErlType where
+    inj [] = ErlNull
+    inj xs = ErlList . map inj $ xs
+
+    prj ErlNull      = Just []
+    prj (ErlList xs) = sequence $ map prj xs
+    prj _            = Nothing
+
+instance (a :< ErlType, b :< ErlType) => (a, b) :< ErlType where
+    inj   (x, y)            = ErlTuple [inj x, inj y]
+    prj (ErlTuple [x, y]) = do
+                                    l <- prj x
+                                    r <- prj y
+                                    return (l, r)
+    prj _ = Nothing
+
+instance (a :< ErlType, b :< ErlType, c :< ErlType) => (a, b, c) :< ErlType where
+    inj   (x, y, z)         = ErlTuple [inj x, inj y, inj z]
+    prj (ErlTuple [x, y, z]) = do
+                                    l <- prj x
+                                    r <- prj y
+                                    f <- prj z
+                                    return (l, r, f)
+    prj _ = Nothing
+
+instance (a :< ErlType) => Maybe a :< ErlType where
+  inj Nothing  = ErlAtom "nothing"
+  inj (Just x) = ErlTuple [ErlAtom "just", inj x]
+
+  prj (ErlAtom "nothing") = Nothing
+  prj (ErlTuple [ErlAtom "just", x]) =
+    do
+      x <- prj x
+      return (Just x)
+  prj _ = Nothing
+
+instance (t :< ErlType) => Erlang t where
+    toErlang = inj
+    fromErlang = fromJust . prj -- unsafe
---
+++ src/SessionCheck/Backend/Erlang/Main.hs
@@ -0,0 +1,93 @@
+module SessionCheck.Backend.Erlang.Main ( erlangMain )where
+import Foreign.Erlang
+import Control.Concurrent.STM
+import Control.Concurrent
+import Control.Monad
+import Data.IORef
+import System.Timeout
+import System.Process
+
+import SessionCheck.Backend
+import SessionCheck.Spec
+import SessionCheck.Test
+
+data Options =
+  Options { targetModule   :: String
+          , targetFunction :: String
+          , erlangNode     :: String
+          , self           :: Self }
+
+-- Should only be called once
+newSession :: String -- ^ Target module
+           -> String -- ^ Target function
+           -> String -- ^ Target node
+           -> IO Options
+newSession mod fun node = do
+  self <- createSelf "haskell@localhost"
+  return $ Options mod fun node self
+
+-- Generate an `Implementation` from the options
+erlang :: Options -> IO (Implementation ErlType)
+erlang opts = do
+  readChan  <- atomically $ newTChan
+  writeChan <- atomically $ newTChan
+  isDead    <- newIORef False
+  return $ Imp { outputChan = readChan
+               , inputChan  = writeChan
+               , dead       = isDead 
+               , run        = runFun readChan writeChan isDead opts }
+
+runFun :: TChan ErlType
+       -> TChan ErlType 
+       -> IORef Bool
+       -> Options
+       -> IO ()
+runFun readChan writeChan dead opts = do
+  mbox <- createMBox (self opts)
+  rpcCall mbox (Short erl) (targetModule opts) (targetFunction opts) []
+  mboxSend mbox (Short erl) (Right "p") (mboxSelf mbox)
+  loop mbox
+  pid <- rpcCall mbox (Short erl) "erlang" "whereis" [ErlAtom "p"]
+  void $ rpcCall mbox (Short erl) "erlang" "exit" [pid, ErlAtom "ok"] 
+  where
+    erl = erlangNode opts
+    loop mbox = do
+      d <- readIORef dead
+      unless d $ do
+        toErlang <- timeout 1000 $ atomically $ readTChan readChan
+        maybe (return ())
+              (\m -> mboxSend mbox
+                             (Short erl)
+                             (Right "p")
+                             (mboxSelf mbox, m)) 
+              toErlang
+        fromErlang <- timeout 1000 $ mboxRecv mbox
+        maybe (return ())
+              (\m -> atomically $ writeTChan writeChan m)
+              fromErlang
+        loop mbox
+
+erlangMain :: String -- ^ On the form "moduleName:functionName"
+           -> Spec ErlType a -- ^ The specification
+           -> IO ()
+erlangMain modfun spec = do
+  let mod = takeWhile (/= ':') modfun
+      fun = tail $ dropWhile (/= ':') modfun
+  ph <- spawnCommand "erl -sname erl > /dev/null"
+  threadDelay 2000000
+  options <- newSession mod fun "erl"
+  imp <- erlang options
+  sessionCheck imp spec 
+  killErlangNode "erl"
+
+-- Kill an erlang node
+killErlangNode :: String -> IO ()
+killErlangNode name = do
+  port <- readCreateProcess
+    (shell $ "epmd -names | awk -v name=" ++ name ++ " '$2==name {print $5}'")
+    ""
+  pid <- readCreateProcess
+    (shell $ "lsof -i TCP:" ++ port
+           ++ " -s TCP:LISTEN | tail -n +2 | awk '{print $2}'")
+    ""
+  void $ createProcess (shell $ "kill " ++ pid) 
