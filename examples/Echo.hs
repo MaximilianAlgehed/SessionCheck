caca1a8fc0e766c2ee939eb34e4c0b0b7eed354d
Maximilian Algehed <m.algehed@gmail.com>
2017-09-11 13:51:20 +0200

    implemented backend + added echo example. Next will test echo implementation

---
+++ examples/Echo.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE TypeOperators, FlexibleContexts #-}
+module Echo where
+
+import GHC.Generics
+import Control.Monad
+import Test.QuickCheck
+import Control.DeepSeq
+import Text.ParserCombinators.ReadP hiding (get)
+import qualified Text.ParserCombinators.ReadP as P
+
+import SessionCheck
+import SessionCheck.Backend.TCP
+
+echo :: (String :< t) => Spec t ()
+echo = do
+  s <- send alphaNumString
+  get (is s)
+  return ()
