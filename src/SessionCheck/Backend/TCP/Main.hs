604b7fa1329e0b48904eef0ec54381c51f7dd0f0
Maximilian Algehed <m.algehed@gmail.com>
2017-09-11 13:30:08 +0200

    skeleton implementation for TCP runfun in place

--- src/SessionCheck/Backend/TCP/Main.hs
+++ src/SessionCheck/Backend/TCP/Main.hs
@@ -2,11 +2,19 @@
 module SessionCheck.Backend.TCP.Main ( tcpMain )where
 
 import SessionCheck.Spec (Spec)
+import SessionCheck.Backend
 import SessionCheck.Backend.TCP.Types
 
 import qualified Data.ByteString as BS
 import Data.ByteString.Internal (c2w, w2c)
 import Network.Simple.TCP
+import Control.Concurrent.STM
+import Control.Concurrent
+import Control.Concurrent.MVar
+import Control.Monad
+import Data.IORef
+import System.Timeout
+import System.Process
 
 -- Convert a String to an effing ByteString
 pack :: String -> BS.ByteString
@@ -21,7 +29,7 @@ unpack = map w2c . BS.unpack
 write :: Socket -> TCPMessage -> IO ()
 write s = send s . pack . (++"\r\n") . unTCPMessage
 
--- Read a message from a socket, incrementally reads individual
+-- Read a message from a socket, works by incrementally reading individual
 -- bytes until it encounters a <CR><LF> pair
 read :: Socket -> IO (Maybe TCPMessage)
 read s = go BS.empty >>= return . fmap (TCPMessage . reverse . unpack)
@@ -43,9 +51,31 @@ read s = go BS.empty >>= return . fmap (TCPMessage . reverse . unpack)
           else
             go (BS.append bs b)
 
+data Options = Options { program :: String } deriving (Ord, Eq, Show)
+
+runFun :: Options
+       -> Implementation TCPMessage
+       -> IO ()
+runFun opts imp = do
+  ph <- spawnCommand $ program opts ++ " > /dev/null"
+  readTid <- listen (Host "localhost") "8080" $ \(sock, sockAdr) -> do
+    readTid <- forkIO $ readThread imp
+    loop imp
+    return readTid
+  terminateProcess ph
+  killThread readTid
+  putMVar (done imp) ()
+  where
+    -- Do the reading
+    readThread imp = return ()
+    -- Do the writing
+    loop imp = return ()
+  
+
 -- TODO: Implement
 tcpMain :: String            -- Program to run
         -> Int               -- Port number
         -> Spec TCPMessage a -- Specification
         -> IO ()
-tcpMain prog port spec = return ()
+tcpMain prog port spec = do
+  return ()
