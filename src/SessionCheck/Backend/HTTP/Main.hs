133fe276fc54e1d653d45ba03f682784efa02229
Maximilian Algehed <m.algehed@gmail.com>
2017-11-15 10:38:03 +0100

    Stuff and shit

--- src/SessionCheck/Backend/HTTP/Main.hs
+++ src/SessionCheck/Backend/HTTP/Main.hs
@@ -2,15 +2,21 @@ module SessionCheck.Backend.HTTP.Main where
 
 import Data.ByteString.Char8 (unpack, pack)
 import qualified Data.ByteString.Lazy.Char8 as L
-import Network.HTTP.Client hiding (host)
+import Network.HTTP.Client hiding (host, port)
 import Network.HTTP.Types.Status
-import Data.CaseInsensitive
+import qualified Network.Wai as Wai
+import Network.Wai.Handler.Warp hiding (run)
+import qualified Network.Wai.Handler.Warp as Warp
+import Data.CaseInsensitive hiding (map)
 import Control.Monad
 import Control.Concurrent.STM
 import Control.Concurrent
 import Control.Concurrent.MVar
 import Data.IORef
 import System.Timeout
+import Data.Char
+import Control.Exception
+import System.Process
 
 import SessionCheck.Backend.HTTP.Types hiding (requestHeaders, requestBody)
 import qualified SessionCheck.Backend.HTTP.Types as T
@@ -37,7 +43,14 @@ issueRequest manager host req = do
                      , replyStatus  = StatusCode (statusCode $ responseStatus response)
                      , replyHeaders = [ (unpack (original n), unpack v) | (n, v) <- responseHeaders response ] }
 
-data Options = Options { host :: Host }
+data Options = Options { host    :: Host
+                       , port    :: Port
+                       , program :: String }
+
+defaultOptions :: Options
+defaultOptions = Options { host    = "http://localhost"
+                         , port    = 8081
+                         , program = "echo \"Hi\"" }
 
 runClient :: Options
           -> Implementation HTTPMessage
@@ -53,19 +66,45 @@ runClient opts imp = do
         mr <- timeout 1000 $ atomically $ readTChan (outputChan imp)
         case mr of
           Just (Request r) -> do
-            reply <- issueRequest manager (host opts) r
+            reply <- issueRequest manager (host opts ++ ":" ++ show (port opts)) r
             atomically $ writeTChan (inputChan imp) (Reply reply)
             loop manager 
-          Just (Reply _) -> (kill imp (Bad "Specification does not follow request-reply structure"))
+          Just (Reply _) -> kill imp (Bad "Specification does not follow request-reply structure")
           _ -> loop manager
 
+runServer :: Options
+          -> Implementation HTTPMessage
+          -> IO ()
+runServer opts imp = undefined
+
 httpMain :: ProtocolRole
-        -> Host -- Host to either test or bind to
-        -> Spec HTTPMessage a -- Specification
-        -> IO ()
-httpMain r h spec = case r of
-  Server -> putStrLn "Server mocking is not yet implemented"
+         -> Options
+         -> Spec HTTPMessage a -- Specification
+         -> IO ()
+httpMain r opts spec = case r of
+  Server -> do
+    imp <- clean
+    tid <- forkIO $ Warp.run (port opts) (runfun imp)
+    sessionCheck (imp { run = void $ spawnCommand (program opts ++ " > /dev/null")  }) spec
+    killThread tid
+    where
+      runfun imp req cont = do
+        body <- Wai.strictRequestBody req
+        atomically . writeTChan (inputChan imp) . Request $ HTTPRequest (read . map toUpper . unpack . Wai.requestMethod $ req)
+                                                                        (unpack . Wai.rawPathInfo $ req)
+                                                                        ([(map toLower $ unpack (original n), unpack v) | (n, v) <- Wai.requestHeaders req])
+                                                                        (L.unpack body)
+        d <- isDead imp
+        when d $ throwIO (userError "Something went wrong!")
+        m <- atomically $ readTChan (outputChan imp)
+        r <- case m of
+              Reply r -> return $ Wai.responseLBS (mkStatus (let StatusCode s = replyStatus r in s) mempty)
+                                                  [(mk (pack n), pack v) | (n, v) <- replyHeaders r]
+                                                  (L.pack (replyBody r))
+              _ -> do
+                kill imp (Bad "Specification does not follow request-reply structure")
+                throwIO $ userError "Something went wrong!"
+        cont r
   Client -> do
-    let opts = Options { host = h }
     imp <- clean 
     sessionCheck (imp { run = runClient opts imp }) spec
